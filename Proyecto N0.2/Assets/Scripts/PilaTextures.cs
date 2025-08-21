c4fe0c39c94927daf68925d6df27ad8f7202b02f
JocManueh <Jose_manuel.diaz@uao.edu.co>
2025-08-20 20:12:35 -0500

    class N1
    pepito

---
+++ Proyecto N0.2/Assets/Scripts/PilaTextures.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PilaTextures : MonoBehaviour {
+
+    // Pila global para toda la clase
+    Stack<string> pilanombres = new Stack<string>();
+    [SerializeField] private TMP_InputField inputNombre;  
+
+    void Start()
+    {
+        Debug.Log("Pila inicializada, pero vacía");
+    }
+
+    // Método para botón 1
+    public void PushString()
+    {
+        
+        Debug.Log("El elemento del tope es:"+pilanombres.Peek());
+    }
+
+    // Método para botón 2
+    public void PeekString()
+    {
+        if (pilanombres.Count > 0)
+        {
+            Debug.Log("Elemento en el tope de la pila: " + pilanombres.Peek());
+        }
+        else
+        {
+            Debug.Log("La pila está vacía (Peek).");
+        }
+    }
+
+    // Método para botón 3
+    public void PopString()
+    {
+        if (pilanombres.Count > 0)
+        {
+            string eliminado = pilanombres.Pop();
+            Debug.Log("Se eliminó de la pila: " + eliminado);
+        }
+        else
+        {
+            Debug.Log("La pila está vacía (Pop).");
+        }
+    }
+}
+
+
+
+
+
+
